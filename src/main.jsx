a1b3e340d402dae8eaa95dc78fc73e49b2007866
Mark-trustaila <mark@trustaila.com>
2026-02-18 16:05:38 +0000

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import LOCS23Assessment from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LOCS23Assessment />
-  </React.StrictMode>
-)
